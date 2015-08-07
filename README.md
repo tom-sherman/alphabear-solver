47e7d9acf2254e5438ce7ec0cd86007d8bfbb072
Tom Sherman <the.tomsherman@gmail.com>
2015-08-07 17:12:12 +0100

    Resolves #2

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # Alphabear Solver
 
-A simple python script that is intended to be used to solve game boards on the mobile game 
+A simple python script that is intended to be used to solve game boards on the 
+mobile game 
 [Alphabear](https://play.google.com/store/apps/details?id=com.spryfox.alphabear&hl=en) by Spry Fox.
 
-Can also be used as a [Countdown](https://en.wikipedia.org/wiki/Countdown_(game_show)#Letters_round) with an arbitrary number of letters, not just 8 like in the game show.
+Can also be used as a 
+[Countdown](https://en.wikipedia.org/wiki/Countdown_(game_show)#Letters_round) 
+with an arbitrary number of letters, not just 8 like in the game show.
 
-Uses a dictionary, generated to `map.json`, from `dictionary.txt`. It's the "official" [SOWPODS](https://en.wikipedia.org/wiki/SOWPODS) dictionary.
+Uses a dictionary, generated to `map.json`, from `dictionary.txt`. It's the 
+"official" [SOWPODS](https://en.wikipedia.org/wiki/SOWPODS) dictionary with a 
+few changes from the Alphabear changelogs.
 
 ## Usage
 
 To use this script, you'll need Python 3 installed.
 
-1. Make sure you have a tone of words in `dictionary.txt`, one word per line. A wordlist is included here but you can modify it if you want.
+1. Make sure you have a tone of words in `dictionary.txt`, one word per line. A 
+wordlist is included here but you can modify it if you want.
 2. Run `hashdict.py`
 3. Run `solver.py`
 
@@ -20,11 +26,13 @@ Everytime you change/update the dictionary, you'll need to rerun `hashdict.py`
 ## Todo
 
 * Read letters from game input.
-* Weight letters so that ones with less moves remaining, worth more points etc. are preferred by the matching algorithm.
-* Grab better dictionary, preferably the one used in the actual game.
+* Calculate the actual in game points.
 
 ### References
 
-* Dictionary (`dictionary.txt`) is taken from here: https://code.google.com/p/scrabblehelper/source/browse/trunk/ScrabbleHelper/src/dictionaries/sowpods.txt?r=20
-* [The World's Fastest Scrabble Program](http://www.cs.cmu.edu/afs/cs/academic/class/15451-s06/www/lectures/scrabble.pdf) - 
+* Dictionary (`dictionary.txt`) is taken from 
+[here](https://code.google.com/p/scrabblehelper/source/browse/trunk/ScrabbleHelper/src/dictionaries/sowpods.txt?r=20) 
+
+* [The World's Fastest Scrabble Program]
+(http://www.cs.cmu.edu/afs/cs/academic/class/15451-s06/www/lectures/scrabble.pdf) - 
 Andrew W. Appel & Guy J. Jacobson (1988)
--- solver.py
+++ solver.py
@@ -7,69 +7,114 @@ __author__ = "Tom Sherman"
 """
 import hashdict
 import itertools
+import re
+from collections import defaultdict
+import copy
 
 
 def main():
     dmap = hashdict.readjson("map.json")
+    turns = False
+    s = ""
+
+    while s not in ("y", "Y", "n", "N"):
+        s = input("Input turns? (y/n)")
+        if s in ("y", "Y"):
+            turns = True
+        else:
+            turns = False
 
     # Loops until user enters the character "0"
     while True:
-        letters = getletters()
+        letters = getletters(turns)
+
         if "0" in letters:
             break
         else:
+
             # Print letters so user can check the inputs
             print("\nYour letters are")
             for L in letters:
                 print(L, end=" ")
             print()
 
-            signatures = getsignatures(letters)
-            printmatches(dmap, signatures)
+            if turns:
+                parseturnes(letters, dmap)
+            else:
+                signatures = getsignatures(letters)
+                matches = getmatches(dmap, signatures)
+                printmatches(matches)
 
 
-def getletters():
+def getletters(turns=False):
     """
         Returns a sanitised, sorted list of letters.
 
+    :param turns: (bool) Whether the user should input them amount of turns.
     :return letters:
     """
 
-    letters = []
+    if turns:
+        print("Syntax: [1];[2];[3];...[n] \n"
+              "Where [1], [2] ... [n] are a list of letters (no spaces) with "
+              "that amount of turns left.")
+        arg = input("Input letters: ")
 
-    s = input("Input letters (no spaces): ")
+        letterssorted = []
+        if validsyntax(arg):
+            # Gets a list of letter groups
+            lettergroups = arg.split(";")
+            # Each letter group is sorted
 
-    for letter in sorted(s):
-        letters.append(letter)
+            # Sort the groups
+            for letters in lettergroups:
+                letterssorted.append(sorted(letters))
 
-    return letters
+            return letterssorted
+        else:
+            letterssorted.append("0")
+            return letterssorted
+
+    else:
+        s = input("Input letters (no spaces): ")
+
+        letters = []
+        for letter in sorted(s):
+            letters.append(letter)
+
+        return letters
 
 
-def printmatches(dmap, signatures):
+def getmatches(dmap, signatures):
     """
-        Actually finds and prints the matches.
-
 
     :param dmap:
     :param signatures:
+    :return matches:
+    """
+
+    matches = []
+
+    for signature in signatures:
+        try:
+            matches = matches + dmap[signature]
+        except KeyError:
+            pass
+
+    return matches
+
+
+def printmatches(matches):
+    """
+    :param matches:
     """
 
     print()
     print("Length | Words")
-    print("--------------")
+    print("-------|------")
 
-    for signature in signatures:
-        try:
-            matches = dmap[signature]
-
-            print("{0:6} | ".format(len(matches[0])), end="")
-
-            for match in matches:
-                print(match, end=" ")
-            print()
-
-        except KeyError:
-            pass
+    for match in matches:
+        print("{0:6} | {1}".format(len(match), match))
 
 
 def getsignatures(letters):
@@ -103,13 +148,12 @@ def getsignatures(letters):
     signature = "".join(sorted(signature))
     signatures.append(signature)
 
-    #print(signatures)
     return removedupes(signatures)
 
 
 def removedupes(lst):
     """
-        Removes all duplicates from a list
+        Returns all duplicates from a list
 
     :param lst:
     :return unique_list:
@@ -124,6 +168,110 @@ def removedupes(lst):
             output.append(value)
             seen.add(value)
     return output
+
+
+def validsyntax(arg):
+    """
+        Validates syntax used in parseturns.
+
+    :param arg: String to be checked.
+    :return bool: True if valid syntax
+    """
+    arg = "<" + arg + ">"
+    r = re.compile(r"<(([a-z]+|\s?);)+[a-z]*>")
+
+    if r.search(arg) is None:
+        return False
+    else:
+        return True
+
+
+def parseturnes(lsletters, dmap):
+    """
+        Used when you want to use the 'turns' syntax.
+
+        Prints the top 3 words to play, calculates using getscore.
+
+    :param lsletters:
+    """
+    letters = []
+    turnmap = getturnmap(lsletters)
+
+    for ls in lsletters:
+        for l in ls:
+            letters.append(l)
+
+    letters = sorted(letters)
+    signatures = getsignatures(letters)
+
+    matches = getmatches(dmap, signatures)
+    printmatches(matches)
+
+    scoredict = defaultdict(list)
+    for match in matches:
+        score = getscore(turnmap, match)
+
+        scoredict[score].append(match)
+
+    print("Top 3 words:")
+
+    counter = 0
+    for score in reversed(sorted(scoredict)):
+        if counter == 3:
+            break
+        print("{0}. {1}".format(counter, scoredict[score]))
+        counter += 1
+
+
+def getturnmap(lsletters):
+    """
+        Returns a map with the score as the key, and the words with that score
+        as the corresponding values.
+
+    :param lsletters:
+    :return turnmap:
+    """
+    index = 1
+
+    turnmap = defaultdict(list)
+    for ls in lsletters:
+        for letter in ls:
+            if letter != " ":
+                turnmap[index].append(letter)
+        index += 1
+
+    return turnmap
+
+
+def getscore(turnmap, match):
+    """
+        Gets the score of each word.
+        This isn't the actual game score, just a number used to rank the words.
+
+    :param turnmap:
+    :param match:
+    :return score:
+    """
+    score = 0
+    tm = copy.deepcopy(turnmap)  # Create copy.
+
+    for c in match:
+        for k in tm:
+            if c in tm[k]:
+                # Removes first character
+                match = match[1:]
+                tm[k].remove(c)
+
+                if k == 1:
+                    score += 100
+                elif k == 2:
+                    score += 6
+                elif k == 3:
+                    score += 5
+                else:
+                    score += 1
+
+    return score
 
 
 if __name__ == '__main__':
